52bd431745ab3a49fee5f14b0c43ef958f40164d
rikli <rik.li@hotmail.com>
2025-06-22 15:45:21 +0800

    data-communication.

--- src/main/java/mqtt/MqttConfig.java
+++ src/main/java/mqtt/MqttConfig.java
@@ -7,7 +7,7 @@ public class MqttConfig {
     private String brokerUrl = "tcp://192.168.0.200:1883";
     private String clientId = "mqtt-client-java";
     private String username = "emqx";
-    private String password = "Root@135799999";
+    private String password = "Root@123456";//如果Broker启用了用户验证，则需要匹配用户信息
     private String topic = "neuron/mqtt/pressure";
     private int qos = 0;
     private boolean cleanSession = true;
