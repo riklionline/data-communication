7955193282cb1586ceba7130075d9fdb800bfb32
rikli <rik.li@hotmail.com>
2025-06-22 15:32:02 +0800

    data-communication.

--- src/main/java/mqtt/MqttClientApplication.java
+++ src/main/java/mqtt/MqttClientApplication.java
@@ -6,15 +6,21 @@ import lombok.extern.slf4j.Slf4j;
 public class MqttClientApplication {
     public static void main(String[] args) {
         try {
-            // 1. 创建配置
+            // 1.创建配置
             MqttConfig config = new MqttConfig();
 
-            // 2. 创建消息处理器
+            // 2.创建消息处理器
             MqttMessageCallback messageProcessor = new SampleMessageProcessor();
 
-            // 3. 创建并启动MQTT客户端(线程池大小为10)
+            // 3.创建并启动MQTT客户端(线程池大小为10)
             MqttClientManager clientManager = new MqttClientManager(config, messageProcessor, 10);
             clientManager.connect();
+
+            // 4.发布主题
+            clientManager.publish("neuron/mqtt/pressure","{\"node\": \"plc_data_terminal\", " +
+                    "\"group\": \"Pressure\", \"timestamp\": 1750572347834, \"values\": {\"DevPressure_0\": 31.98, " +
+                    "\"DevPressure_1\": 95.95, \"DevPressure_2\": 63.97, \"DevPressure_3\": 51.18, \"DevPressure_4\": 38.38}",
+                    0,true);
 
             // 添加关闭钩子
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
--- src/main/java/mqtt/MqttConfig.java
+++ src/main/java/mqtt/MqttConfig.java
@@ -7,7 +7,7 @@ public class MqttConfig {
     private String brokerUrl = "tcp://192.168.0.200:1883";
     private String clientId = "mqtt-client-java";
     private String username = "emqx";
-    private String password = "Root@13579";
+    private String password = "Root@135799999";
     private String topic = "neuron/mqtt/pressure";
     private int qos = 0;
     private boolean cleanSession = true;
